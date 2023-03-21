8cf3054d33759b32e6a31afd7412536c6893d053
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-21 00:11:02 -0400

    Adds useEffect and axios call processing

---
+++ pages/category/[id].jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+
+
+
+export default function Quiz() {
+  const router = useRouter()
+  const [questions, setQuestions] = useState(null)
+  const [questionNum, setQuestionNum] = useState(0)
+  const category = useRef(null)
+
+  useEffect(() => {
+    if (!router.isReady) return;
+  
+    const categoryID = router.query.id
+    
+    const URL = `https://opentdb.com/api.php?amount=10&category=${categoryID}`
+    console.log(URL);
+    axios.get(URL).then((response) => {
+      console.log(response.data)
+      category.current = response.data.results[0].category
+      let cleanResult = response.data.results
+
+      //Make an array of questions and shuffle them
+      for (const question of cleanResult){
+        question.answers = [question.correct_answer ]
+        question['incorrect_answers'].forEach(element => { question.answers.push(element) });
+        question.answers = shuffle(question.answers)
+      }
+      setQuestions(cleanResult)
+      
+    })
+  }, [router.isReady])
+  
+
+  return (<>
+    <div className="d-flex justify-content-between  flex-direction: column">
+      <h1>{category.current}</h1>
+      <h1>Question {questionNum + 1}/10</h1>
+    </div>
+    </>
+  )
+}
+
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
